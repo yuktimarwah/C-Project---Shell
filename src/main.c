ff0edba570ce826b7280804384a383a2f7ade4d6
Yukti Marwah <yuktimarwahgithub@gmail.com>
2026-09-18 12:44:00 +0000

    Implement interactive shell loop

--- src/main.c
+++ src/main.c
@@ -1,7 +1,17 @@
 #include <stdio.h>
 
-int main(void)
-{
-    printf("PipeDream\n");
-    return 0;
+int main (void) {
+
+	char command[100];
+
+	while (1) {
+
+		printf("PipeDream$ ");
+
+		fgets(command, 100, stdin);
+
+		printf("You entered: %s", command);
+	}
+
+	return 0;
 }
